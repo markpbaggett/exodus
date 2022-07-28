3096ead7f9e8e6b5a052c82c7a5af7f9e355b408
Mark Baggett <mbagget1@utk.edu>
2022-07-28 14:18:53 -0400

    Reconfigure Property for multiple xpaths.

--- exodus/exodus.py
+++ exodus/exodus.py
@@ -1,4 +1,5 @@
 from lxml import etree
+import yaml
 
 
 class Property:
@@ -8,11 +9,17 @@ class Property:
         self.root = etree.parse(path)
         self.root_as_str = etree.tostring(self.root)
 
-    def find(self, xpath):
-        all_identifiers = self.root.xpath(xpath, namespaces=self.namespaces)
-        for identifier in all_identifiers:
-            print(identifier.text)
+    def find(self, xpaths):
+        all_values = []
+        for xpath in xpaths:
+            matches = self.root.xpath(xpath, namespaces=self.namespaces)
+            for match in matches:
+                all_values.append(match.text)
+        return all_values
 
 
 if __name__ == "__main__":
-    Property('fixtures/arrow_1.xml', {"mods": "http://www.loc.gov/mods/v3"}).find('//mods:identifier[@type="pid"]')
+    x = yaml.safe_load(open("config.yml", "r"))
+    local_identifiers = Property('fixtures/arrow_1.xml', {"mods": "http://www.loc.gov/mods/v3"}).find(x['local_name']['xpaths'])
+    print(local_identifiers)
+
