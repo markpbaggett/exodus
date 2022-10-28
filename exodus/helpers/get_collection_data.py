dcbf2f832d9c556d81fe50cdd235ee1e68d20c14
Mark Baggett <mbagget1@utk.edu>
2022-10-28 10:44:00 -0500

    Init adding collections to output csv.

--- exodus/helpers/get_collection_data.py
+++ exodus/helpers/get_collection_data.py
@@ -1,10 +1,13 @@
 import requests
 from lxml import etree
 from io import BytesIO
+import csv
+import tqdm
 
 
 class CollectionMetadata:
     def __init__(self, pid):
+        self.pid = pid
         self.mods = self.__get_metadata(pid)
         self.namespaces = {
             "mods": "http://www.loc.gov/mods/v3",
@@ -12,10 +15,13 @@ class CollectionMetadata:
         }
 
     def __simplify_xpath(self, xpath):
-        return [value.text for value in self.mods.xpath(xpath, namespaces=self.namespaces)]
+        return "|".join([value.text for value in self.mods.xpath(xpath, namespaces=self.namespaces)])
 
-    def __grab_all(self):
+    def grab_all_metadata(self):
         return {
+            "source_identifier": self.pid,
+            "model": "Collection",
+            "parents": "",
             "title": self.__simplify_xpath('mods:titleInfo/mods:title'),
             "abstract": self.__simplify_xpath('mods:abstract'),
             "contributor": "",
@@ -50,5 +56,51 @@ class CollectionMetadata:
         return [value.text for value in self.mods.xpath('mods:name[mods:role/mods:roleTerm[contains(.,"Photographer")]]/mods:namePart', namespaces=self.namespaces)]
 
 
+class CollectionOrganizer:
+    def __init__(self, csv):
+        self.original_csv = csv
+        self.original_as_dict = self.__read()
+        self.unique_collections = self.__build_collections()
+        self.headers = self.__get_headers()
+
+    def __read(self):
+        csv_content = []
+        with open(self.original_csv, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                csv_content.append(row)
+        return csv_content
+
+    def __get_headers(self):
+        original_headers = [k for k, v in self.original_as_dict[0].items()]
+        collection_headers = [k for k, v in self.unique_collections[0].items()]
+        for header in collection_headers:
+            if header not in original_headers:
+                original_headers.append(header)
+        return original_headers
+
+    def __get_unique_collections(self):
+        unique_collections = []
+        work_types = ('Image', 'Video', 'Audio')
+        for thing in self.original_as_dict:
+            if thing['model'] in work_types:
+                if thing['parents'] not in unique_collections:
+                    unique_collections.append(thing['parents'])
+        return unique_collections
+
+    def __build_collections(self):
+        return [CollectionMetadata(collection).grab_all_metadata() for collection in self.__get_unique_collections()]
+
+    def write_csv(self, filename):
+        with open(filename, 'w', newline='') as bulkrax_sheet:
+            writer = csv.DictWriter(bulkrax_sheet, fieldnames=self.headers)
+            writer.writeheader()
+            for data in self.original_as_dict:
+                writer.writerow(data)
+            for data in self.unique_collections:
+                writer.writerow(data)
+        return
+
+
 if __name__ == "__main__":
-    print(CollectionMetadata('collections:boydcs').get_keyword())
+    CollectionOrganizer('temp/shana_boydcs_with_filesets_and_attachments.csv').write_csv('temp/shana_boydcs_with_collections.csv')
