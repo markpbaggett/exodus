fec710d5ec1b594a416250737a5639b6caa910ec
Mark Baggett <mbagget1@utk.edu>
2022-12-30 10:47:00 -0500

    Generate csv import based on failures in an import.

---
+++ exodus/helpers/copy_failures_from_original_import_file.py
@@ -0,0 +1,42 @@
+import csv
+
+
+class FailuresFinder:
+    def __init__(self, original_csv, failed_imports):
+        self.original_csv = original_csv
+        self.failures = self.__get_failures_only(original_csv, failed_imports)
+        self.headers = self.__get_headers()
+
+    def __get_headers(self):
+        original_headers = [k for k, v in self.failures[0].items()]
+        return original_headers
+
+    @staticmethod
+    def __get_failures_only(original, failures):
+        csv_content = []
+        failed_to_import = []
+        with open(failures, 'r') as failures_file:
+            for line in failures_file:
+                failed_to_import.append(line.strip())
+        with open(original, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row['source_identifier'] in failed_to_import:
+                    csv_content.append(row)
+        return csv_content
+
+    def write_new_import_file_with_failures_only(self, filename, newline=''):
+        with open(filename, 'w', newline=newline) as bulkrax_sheet:
+            writer = csv.DictWriter(bulkrax_sheet, fieldnames=self.headers)
+            writer.writeheader()
+            for data in self.failures:
+                writer.writerow(data)
+        return
+
+
+if __name__ == "__main__":
+    x = FailuresFinder(
+        'migrations/roth_full_just_filesets_and_attachments_with_filesheets_and_attachments_only_6.csv',
+        'temp/91.txt'
+    )
+    x.write_new_import_file_with_failures_only('temp/91.csv')
