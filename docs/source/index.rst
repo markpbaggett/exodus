baa8c89d259737bffc64c6a886cc58dce3aca53a
Mark Baggett <mbagget1@utk.edu>
2023-01-18 11:07:43 -0500

    Init docs.

---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. Exodus documentation master file, created by
+   sphinx-quickstart on Wed Jan 18 10:55:06 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Exodus's documentation!
+==================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as read_me:
+    long_description = read_me.read()
+
+
+setup(
+    name="Exodus Procedures",
+    description="Documentation generation for describing migration procedures from Islandora 7 to Hyku",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version="0.0.1",
+    author="Mark Baggett",
+    author_email="mbagget1@utk.edu",
+    maintainer_email="mbagget1@utk.edu",
+    url="https://github.com/markpbaggett/crossref_batch_",
+    packages=find_packages(),
+    extras_require={
+        "docs": [
+            "sphinx >= 5.0.2",
+            "sphinx-rtd-theme >= 1.0.0",
+        ]
+    },
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+        "Topic :: Multimedia :: Graphics :: Presentation",
+    ],
+    keywords=["libraries", "documentation", "hyku"],
+)
